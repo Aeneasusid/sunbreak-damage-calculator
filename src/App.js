042f39769ec496b90d750beb62a934a7e33aaf77
Aeneasusid <aeneasusid@gmail.com>
2023-01-14 02:13:21 +0800

    Log 0.1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
+import AttackPower from "./components/AttackPower";
+import ElementValue from "./components/ElementValue";
 import './App.css';
 
 class App extends React.Component {
-    constructor() {
-        super();
+    state = {
+        skills:{
+            attackPower:[[true],[1,1],[1,1],[1,1],[1.05,1],[1.06,1],[1.08,1],[1.1,10]],
+            longBarrel:[[[true],[1.05,0],[1.075,0]],    [[false],[1.05,0],[1.075,0]]],
+            elementAttack:[[false],[1,1],[1,1],[1,1],[1.05,1],[1.2,4]],
+            burst:[[[true],[0,8],[0,9],[0,10]],    [[false],[0,5],[0,5],[0,5]],     [1]],
+        },
     }
 
     render() {
         return (
-            <div>
-                MH Rise: Sunbreak - Damage Calculator
+            <div class="ui container">
+                <h1 className="ui huge header center aligned purple">MH Rise: Sunbreak - Damage Calculator</h1>
+                <div className="ui grid">
+                    <AttackPower skills={this.state.skills} />
+                    <ElementValue skills={this.state.skills} />
+                </div>
             </div>
         );
     }
